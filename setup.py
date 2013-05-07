2ac6afa57a2af6e73fa320d9de758fa1c93eb7f5
Lars Kreisz <der.kraiz@gmail.com>
2013-05-07 22:37:25 +0200

    version bump to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='hamster-bridge',
     description='let your hamster log your work to your favorite bugtracker',
-    version='0.0.1',
+    version='0.1.0',
     author='Lars Kreisz',
     author_email='der.kraiz@gmail.com',
     license='MIT',
