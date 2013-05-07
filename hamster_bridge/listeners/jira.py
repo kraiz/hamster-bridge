1779ac423c617cba28c97c66b39059ab796692bc
Lars Kreisz <der.kraiz@gmail.com>
2013-05-07 22:35:32 +0200

    refactored jira issue name extraction

--- hamster_bridge/listeners/jira.py
+++ hamster_bridge/listeners/jira.py
@@ -35,23 +35,28 @@ class JiraHamsterListener(HamsterListener):
         except:
             logger.exception('Can not connect to JIRA, please check hamster-bridge.cfg')
 
+    def __issue_from_fact(self, fact):
+        """
+        Get the issue name from a fact
+        :param fact: the fact to search the issue in
+        """
+        for possible_issue in self.issue_from_title.findall(fact.activity):
+            try:
+                self.jira.issue(possible_issue)
+                return possible_issue
+            except JIRAError, e:
+                if e.text == 'Issue Does Not Exist':
+                    logger.warning('Tried Issue "%s", but does not exist. ', fact.activity)
+                    continue
+                else:
+                    raise e
+        return None
+
     def on_fact_stopped(self, fact):
         time_spent = '%dm' % (fact.delta.total_seconds() / 60)
 
         try:
-            issue_name = None
-            for possible_issue in self.issue_from_title.findall(fact.activity):
-                try:
-                    self.jira.issue(possible_issue)
-                    issue_name = possible_issue
-                    break
-                except JIRAError, e:
-                    if e.text == 'Issue Does Not Exist':
-                        logger.warning('Tried Issue "%s", but does not exist. ', fact.activity)
-                        continue
-                    else:
-                        raise e
-
+            issue_name = self.__issue_from_fact(fact)
             if issue_name is not None:
                 self.jira.add_worklog(issue_name, time_spent)
                 logger.info('Logged work: %s to %s', time_spent, issue_name)
