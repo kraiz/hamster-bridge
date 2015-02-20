51b6cb3b67f6868ab76c04aa8db85b1c1d4cd224
kraiz <lars.kreisz@gmail.com>
2015-02-20 20:11:23 +0100

    switched from "jira-python" to "jira"
    BEWARE: untested code, need to verify next week

--- setup.py
+++ setup.py
@@ -5,16 +5,13 @@ from setuptools import setup
 setup(
     name='hamster-bridge',
     description='let your hamster log your work to your favorite bugtracker',
-    version='0.4.0',
+    version='0.5.0a1',
     author='Lars Kreisz',
     author_email='der.kraiz@gmail.com',
     license='MIT',
     url='https://github.com/kraiz/hamster-bridge',
     packages=['hamster_bridge', 'hamster_bridge.listeners'],
     entry_points={'console_scripts': ['hamster-bridge = hamster_bridge:main']},
-    long_description=open('README.rst').read(),
-    install_requires = [
-        'oauthlib>0.3.7',
-        'jira-python>=0.13'
-    ]
+    long_description=open('README.rst').read() + '\n\n' + open('CHANGELOG.rst').read(),
+    install_requires=['jira>=0.40']
 )
