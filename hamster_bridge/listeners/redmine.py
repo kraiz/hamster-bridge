8ff779bee52bd9b8c12e72dec7db574d37f32d39
Alexander Herrmann <darignac@gmail.com>
2015-03-01 14:12:11 +0100

    rewrote issue stopping removed code we do not need any more

--- hamster_bridge/listeners/redmine.py
+++ hamster_bridge/listeners/redmine.py
@@ -1,9 +1,7 @@
 from __future__ import absolute_import
 
-import json
 import logging
 import re
-import requests
 
 from hamster_bridge.listeners import HamsterListener
 
@@ -20,7 +18,7 @@ logger = logging.getLogger(__name__)
 class RedmineHamsterListener(HamsterListener):
     """
     Redmine listener for hamster tasks,
-    Tested with Redmine 2.2.2.stable
+    Tested with Redmine 2.5.1.stable
 
     Important: will only work with German or English installation!
 
@@ -56,16 +54,9 @@ class RedmineHamsterListener(HamsterListener):
         """
         Sets up the class be defining some internal variables.
         """
-        # FIXME still necessary?
-        # will store the issue statuses as returned by Redmine API during the call of RedmineHamsterListener.prepare
-        self.__issues_statuses = []
-        # FIXME still necessary?
-        # will store the issue data, will be updated per issue whenever an issue is requested on the API
-        self.__issues = {}
-        # FIXME still necessary?
         # issue status dict for the default issue status
         self.__issue_status_default = None
-        # FIXME still necessary?
+
         # issue status dict for the "in Work" status
         self.__issue_status_in_work = None
 
@@ -74,113 +65,6 @@ class RedmineHamsterListener(HamsterListener):
 
         # will store the activities
         self.__activities = {}
-
-        # will store the currently active issue
-        self.issue = None
-
-    # FIXME still necessary?
-    def __request_resource(self, resource, method='get', data=None):
-        """
-        Request the given resource from the Redmine API using the given method.
-        If method is PUT or POST, also sent the given data.
-
-        :param resource: the URL to call on Redmine API
-        :type resource: str
-        :param method: the HTTP method
-        :type method: str
-        :param data: the data to put if method is PUT or POST
-        :type data: str
-        """
-        kwargs = {
-            'headers': {
-                'X-Redmine-API-Key': self.config.get(self.short_name, 'api_key'),
-                'content-type': 'application/json',
-            },
-            'verify': True if self.config.get(self.short_name, 'verify_ssl') == 'y' else False,
-        }
-
-        if data is not None and (method == 'put' or method == 'post'):
-            kwargs['data'] = data
-
-        url = self.config.get(self.short_name, 'server_url')
-        if not url.endswith('/'):
-            url += '/'
-
-        return getattr(requests, method)(
-            '%(url)s%(resource)s' % {
-                'url': url,
-                'resource': resource,
-            },
-            **kwargs
-        )
-
-    # FIXME still necessary?
-    def __update_issue(self, issue_id, data):
-        """
-        Updates the issue with the given id with the given data.
-
-        :param issue_id: the id of the issue
-        :type issue_id: str
-        :param data: the data to update
-        :type data: dict
-        """
-        req = self.__request_resource(
-            self.resources['issue'] % {'issue_id': issue_id},
-            method='put',
-            data=json.dumps(data),
-        )
-        if req.status_code != 200:
-            logger.error('Unable to set issue %(issue_id)s to data %(data)s.' % {'issue_id': issue_id, 'data': data})
-
-    # FIXME still necessary?
-    def __log_work(self, issue_number, date_spent_on, time_spent):
-        """
-        Logs work to Redmine with the given values.
-
-        :param issue_number: the issue to log to
-        :type issue_number: str
-        :param date_spent_on: the date the time was spent
-        :type date_spent_on: datetime.date
-        :param time_spent: the amount of time that was spent
-        :type time_spent: str
-        """
-        req = self.__request_resource(
-            self.resources['time_entries'],
-            method='post',
-            data=json.dumps(
-                {
-                    'time_entry': {
-                        'issue_id': issue_number,
-                        'spent_on': date_spent_on.strftime('%Y-%m-%d'),
-                        'hours': time_spent,
-                    }
-                }
-            )
-        )
-        if req.status_code == 201:
-            logger.info('Logged work: %(time_spent)s to %(issue)s on %(date_spent_on)s' % {
-                'time_spent': time_spent,
-                'issue': issue_number,
-                'date_spent_on': date_spent_on,
-            })
-        else:
-            logger.error('Unable to log time to Redmine. HTTP status code was %s with error %s', req.status_code, req.text)
-
-    # FIXME still necessary?
-    def __exists_issue(self, issue_id):
-        """
-        Checks if an issue with the given issue_id exists by calling the Redmine API,
-
-        :param issue_id: the issue id
-        :type issue_id: str
-        """
-        req = self.__request_resource(self.resources['issue'] % {'issue_id': issue_id})
-        if req.status_code == 200:
-            # cache the issue values for later use
-            self.__issues[issue_id] = req.json()['issue']
-            return True
-
-        return False
 
     def __get_issue_from_fact(self, fact):
         """
@@ -273,23 +157,20 @@ class RedmineHamsterListener(HamsterListener):
         # if issue shall be auto started...
         if self.config.get(self.short_name, 'auto_start') == 'y':
             # fetch the issue from the hamster fact
-            self.issue = self.__get_issue_from_fact(fact)
+            issue = self.__get_issue_from_fact(fact)
 
-            if not self.issue:
-                logger.error('Unable to query an issue for the hamster fact %s', fact.original_activity)
+            # abort if no issue was found
+            if not issue:
+                logger.error('Unable to query issue for starting of hamster fact %s', fact.original_activity)
                 return
 
             # if the issue is in the default state (aka the initial state), put it into work state
-            if self.issue.status.id == self.__issue_status_default.id:
-                logger.info('setting status to "In Work" for issue %d', self.issue.id)
-                self.issue.status_id = self.__issue_status_in_work.id
-                self.issue.save()
+            if issue.status.id == self.__issue_status_default.id:
+                logger.info('setting status to "In Work" for issue %d', issue.id)
+                issue.status_id = self.__issue_status_in_work.id
+                issue.save()
 
-    # FIXME still necessary?
     def on_fact_stopped(self, fact):
-        # FIXME remove
-        print('on_fact_stopped', fact)
-        # TODO handle self.issue
         """
         Called by HamsterBridge if a fact is stopped.
         Will try to log the time to the appropriate Redmine issue if there is one.
@@ -298,8 +179,19 @@ class RedmineHamsterListener(HamsterListener):
         :param fact: the currently stopped fact
         :type fact: hamster.lib.stuff.Fact
         """
-        issue_id = self.__get_issue_id_from_fact(fact)
-        if issue_id is not None:
-            self.__log_work(issue_id, fact.date, '%0.2f' % (fact.delta.total_seconds() / 3600.0))
-        else:
-            logger.info('No valid issue found in "%s"', fact.activity)
+
+        # fetch the issue from the hamster fact
+        issue = self.__get_issue_from_fact(fact)
+
+        # abort if no issue was found
+        if not issue:
+            logger.error('Unable to query issue for stopping of hamster fact %s', fact.original_activity)
+            return
+
+        # create the time entry
+        self.redmine.time_entry.create(
+            issue_id=issue.id,
+            hours='%0.2f' % (fact.delta.total_seconds() / 3600.0),
+            # FIXME this grabs the first activity from the dict, it should somehow be defined.... maybe through the tags?
+            activity_id=self.__activities.itervalues().next(),
+        )
