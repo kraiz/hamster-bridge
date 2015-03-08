407dc3262ac520cc37f8e0cfb81bd45712808af2
kraiz <lars.kreisz@gmail.com>
2015-03-08 10:47:14 +0100

    added flag to set the hamster check interval

--- hamster_bridge/__init__.py
+++ hamster_bridge/__init__.py
@@ -27,6 +27,8 @@ def main():
     parser = argparse.ArgumentParser(description='Let your hamster log your work to your favorite bugtracker.')
     parser.add_argument('bugtracker', choices=sorted(listener_choices.keys()))
     parser.add_argument('-d', '--debug', action='store_true', help='enable debug logging')
+    parser.add_argument('-c', '--check-interval', default='2', type=int,
+                        help='check every this amount of seconds for updates')
     args = parser.parse_args()
 
     logging.basicConfig(
@@ -40,7 +42,8 @@ def main():
     logger.debug('Activating listener: %s', args.bugtracker)
     bridge.add_listener(listener_choices[args.bugtracker]())
     bridge.configure()
-    bridge.run()    
+    logger.debug('Run with check interval of %ds', args.check_interval)
+    bridge.run(args.check_interval)
 
 if __name__ == "__main__":
     main()
